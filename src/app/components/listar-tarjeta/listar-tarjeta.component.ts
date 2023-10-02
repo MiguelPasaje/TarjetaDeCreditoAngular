5c924c93a87fa5f48d79b1f0637b263d915a00c4
MiguelPasaje <cartel-dy@hotmail.com>
2023-10-01 21:29:14 -0500

    listar las tarjetas en la card

--- src/app/components/listar-tarjeta/listar-tarjeta.component.ts
+++ src/app/components/listar-tarjeta/listar-tarjeta.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TarjetaCredito } from 'src/app/models/TarjetaCredito';
+import { TarjetaService } from 'src/app/services/tarjeta.service';
 
 @Component({
   selector: 'app-listar-tarjeta',
   templateUrl: './listar-tarjeta.component.html',
-  styleUrls: ['./listar-tarjeta.component.css']
+  styleUrls: ['./listar-tarjeta.component.css'],
 })
-export class ListarTarjetaComponent {
+export class ListarTarjetaComponent implements OnInit {
 
+  listTarjetas:TarjetaCredito[] = []
+
+  constructor(private _tarjetaService: TarjetaService) {}
+
+  ngOnInit(): void {
+    this.obtenerTarjetas()
+  }
+
+  obtenerTarjetas() {
+    this._tarjetaService.obtenerTarjetas().subscribe(data =>{
+      this.listTarjetas = []
+      data.forEach((element:any)=> {
+        this.listTarjetas.push({
+          id:element.payload.doc.id,
+          ...element.payload.doc.data()
+        })
+      })
+      console.log(this.listTarjetas)
+    })
+  }
 }
